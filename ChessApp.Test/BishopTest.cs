74b3b39d40d135d23448d062680524c2c8b86904
unknown <karl.baker@NA-PC-KBAKER3.viagogo.corp>
2014-06-23 13:37:49 -0700

    Actually adding Bishop files this time

---
+++ ChessApp.Test/BishopTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChessApp.Test
+{
+    [TestClass]
+    public class BishopTest
+    {
+        [TestInitialize]
+        public void BeforeEachTest()
+        {
+
+        }
+    }
+}
---
+++ ChessApp/Bishop.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ChessApp
+{
+    public class Bishop : Piece
+    {
+        public override IEnumerable<BoardCoordinate> GetMovesFrom(BoardCoordinate moveCoordinate, int boardSize)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
